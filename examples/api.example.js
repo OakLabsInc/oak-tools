ea5ae80ed42a4b3e7a6f7544ebfddf3309d690df
Flynn Joffray <nucleardreamer@gmail.com>
2017-04-17 09:53:34 -0700

    safe-buffer, package updates

--- examples/api.example.js
+++ examples/api.example.js
@@ -2,7 +2,9 @@ const { join } = require('path')
 const tools = require(join(__dirname, '..'))
 const Api = tools.api
 
-const petstore = new Api('http://petstore.swagger.io/v2/swagger.json', function (err, api) {
+let petstore = new Api('http://petstore.swagger.io/v2/swagger.json', function (err, api) {
   if (err) throw err
   console.log('petStore api:', api)
 })
+
+console.log('petstore return', petstore)
